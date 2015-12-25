93f2244bbba25dfb392f7db1b913da23650e3798
unknown <alexjin9317@outlook.com>
2015-12-26 03:15:00 +0800

    dbconf

--- config/constants.php
+++ config/constants.php
@@ -5,7 +5,7 @@ define('QB_AUTH_KEY', 'uxTpjpJVHYeNfsQ');
 define('QB_AUTH_SECRET', 'thNWXE9SX-eDHME');
 define('QB_USER_PASS', 'thNWXE9SX-eDHME');
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'homerdbinstance.cx1g4poofx9k.us-west-2.rds.amazonaws.com');
 define('DB_NAME', 'homerdbinstance');
 define('DB_USER', 'ivonemaster');
 define('DB_PASS', 'Ivone491409');

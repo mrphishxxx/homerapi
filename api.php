6ec1ab12aef7f65fd4f238b2b3f5a1157077eaeb
Alex--Jin <alexjin9317@outlook.com>
2016-03-25 17:14:33 +0800

    similar fix

--- api.php
+++ api.php
@@ -636,7 +636,7 @@ function api_get_post_detail(){
 					if ($p->dist > 5){
 						continue;
 					}
-					$post = Post::find($p->sid);
+					$post = Post::find($p->post_from);
 					if ($post == NULL){
 						$p->delete();
 						continue;

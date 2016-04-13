f29ffb582dd4d036582c74af23133063aa3e97ab
root <root@ip-172-31-16-246.us-west-2.compute.internal>
2016-04-13 01:49:42 +0000

    folder name

---
+++ config/constants1.php
@@ -0,0 +1,14 @@
+<?php
+
+define('QB_APP_ID', '32839');
+define('QB_AUTH_KEY', 'uxTpjpJVHYeNfsQ');
+define('QB_AUTH_SECRET', 'thNWXE9SX-eDHME');
+define('QB_USER_PASS', 'thNWXE9SX-eDHME');
+
+define('DB_HOST', 'homerdbinstance.c1uvf8ruqi9y.us-west-2.rds.amazonaws.com');
+define('DB_NAME', 'homerdbinstance');
+define('DB_USER', 'ivonemaster');
+define('DB_PASS', 'Ivone491409');
+
+
+?>
